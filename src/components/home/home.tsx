338e6cd631d99062a3f1baae2e7aa856bd05a375
ashok <mpushparaja13@gmail.com>
2024-07-29 21:45:07 +0530

    delete verification pages

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -27,26 +27,6 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 justify-content">
-        <div className="col-md-2">
-          <Link to="/travel-accounts" className="row pointer-cursor g-0 border bgwhite rounded h-140 mb-4 shadow-sm align-center">
-            <div><img width={70} src="https://cdn-icons-png.freepik.com/512/14235/14235158.png?ga=GA1.1.1669094140.1721714955" /></div>
-            <div aria-label="accounts & info" className="mt-3 link">Travel Account</div>
-          </Link>
-        </div>
-        <div className="col-md-2">
-          <Link to="#" className="row pointer-cursor g-0 border bgwhite rounded h-140 mb-4 shadow-sm align-center">
-            <div><img width={70} src="https://cdn-icons-png.freepik.com/512/14229/14229939.png?ga=GA1.1.1669094140.1721714955" /></div>
-            <div aria-label="payment history" className="mt-3 link">Travel insurance</div>
-          </Link>
-        </div>
-        <div className="col-md-2">
-          <Link to="#" className="row pointer-cursor g-0 border bgwhite rounded h-140 mb-4 shadow-sm align-center">
-            <div><img width={70} src="https://cdn-icons-png.freepik.com/512/11275/11275461.png?ga=GA1.1.1669094140.1721714955" /></div>
-            <div aria-label="funds transfer" className="mt-3 link">Loans</div>
-          </Link>
-        </div>
-      </div>
     </div>
   </>
   );
